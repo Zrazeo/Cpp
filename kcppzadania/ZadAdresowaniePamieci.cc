987cd81bfcef871ccc8743b44fac0dfb78769973
Krystian Osmelak <74413287+Zrazeo@users.noreply.github.com>
2021-04-06 20:34:20 +0200

    Add files via upload

---
+++ kcppzadania/ZadAdresowaniePamieci.cc
@@ -0,0 +1,18 @@
+  
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int x, y, z;
+    x = 5;
+    y = 10;
+    z = 15;
+
+    cout << "X: " << &x << " Wartosc: " << x <<  endl;
+    cout << "Y: " << &y << " Wartosc: " << y <<  endl;
+    cout << "Z: " << &z << " Wartosc: " << z <<  endl;
+
+    return 0;
+}
