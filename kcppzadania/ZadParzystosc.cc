b3ef20a6ffc462aba0586f2cc61a50583491550f
Krystian Osmelak <74413287+Zrazeo@users.noreply.github.com>
2021-04-07 10:48:47 +0200

    2

--- kcppzadania/ZadParzystosc.cc
+++ kcppzadania/ZadParzystosc.cc
@@ -1,21 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n = 5;
-    cout << n << endl;
-
+int modulo(int n){
+    
     if ( n % 2 == 0)
         cout << "Liczba parzysta"<< endl;
     else
         cout << "Liczba nieparzysta"<< endl;
+}
+
+int bin(int n){
 
     if(n & 1)  
         cout <<"Liczba nieparzysta"<< endl; 
     else 
         cout <<"Liczba parzysta."<< endl;
+}
 
-    // sprawdza czy 1 bit jest zerem albo jedynką
+int opWarunkowy(int n){
+
+     cout << ((n & 1) ? "Liczba nieparzysta" : "Liczba parzysta");
+}
+
+int main()
+{
+    int n;
+    cout << "Liczba do sprawdzenia: " << endl;
+    cin >> n;
+
+    cout <<"Modulo"<< endl;
+    modulo(n);
+
+    cout << "Za pomoca operatora warunkowego"<< endl;
+    opWarunkowy(n);
+
+    cout << endl << "Za pomoca operatora binarnego"<< endl;
+    bin(n);
+
+    //6 = 0110 & 0001 sprawdza czy pierwszy bit jest zerem lub jedynką
+    // jesli jest jedynka to jest liczba nieparzysta 
     return 0;
 }
